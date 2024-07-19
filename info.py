95eb0ceaf65929e518eb73dfb8dadea8dc6f6007
ak4422920 <ak4422920@gmail.com>
2024-07-19 14:30:23 +0530

    Update info.py

--- info.py
+++ info.py
@@ -18,8 +18,8 @@ def is_enabled(value, default):
 
 # Bot information
 SESSION = environ.get('SESSION', 'TechVJBot')
-API_ID = int(environ.get('API_ID', ''))
-API_HASH = environ.get('API_HASH', '')
+API_ID = int(environ.get('API_ID', '29171167'))
+API_HASH = environ.get('API_HASH', '7ea2149629e445936619f06a3c0dc716')
 BOT_TOKEN = environ.get('BOT_TOKEN', "")
 
 # Bot settings
