a3aefb28d28680b50314109bf3bcb52b081c28c1
amit596 <kaliraman449@gmail.com>
2025-07-18 12:46:01 +0530

    Update info.py

--- info.py
+++ info.py
@@ -114,20 +114,20 @@ USE_CAPTION_FILTER = bool(environ.get('USE_CAPTION_FILTER', True))
 # Token Verification Info :
 VERIFY = bool(environ.get('VERIFY', False))
 VERIFY_SHORTLINK_URL = environ.get('VERIFY_SHORTLINK_URL', 'linkshortify.com')
-VERIFY_SHORTLINK_API = environ.get('VERIFY_SHORTLINK_API', 'c8c51280ae7b73b07b278a6ec131c3fb9d1598c5')
+VERIFY_SHORTLINK_API = environ.get('VERIFY_SHORTLINK_API', 'c1388492d1c69d38d15a0755f49592ff4b174ac4')
 VERIFY_TUTORIAL = environ.get('VERIFY_TUTORIAL', 'https://t.me/how_to_open_link_ak/18')
 
 # If You Fill Second Shortner Then Bot Attach Both First And Second Shortner And Use It For Verify.
 VERIFY_SECOND_SHORTNER = bool(environ.get('VERIFY_SECOND_SHORTNER', False))
 # if verify second shortner is True then fill below url and api
 VERIFY_SND_SHORTLINK_URL = environ.get('VERIFY_SND_SHORTLINK_URL', 'linkshortify.com')
-VERIFY_SND_SHORTLINK_API = environ.get('VERIFY_SND_SHORTLINK_API', 'c8c51280ae7b73b07b278a6ec131c3fb9d1598c5')
+VERIFY_SND_SHORTLINK_API = environ.get('VERIFY_SND_SHORTLINK_API', 'c1388492d1c69d38d15a0755f49592ff4b174ac4')
 
 
 # Shortlink Info
 SHORTLINK_MODE = bool(environ.get('SHORTLINK_MODE', True)) # Set True Or False
 SHORTLINK_URL = environ.get('SHORTLINK_URL', 'linkshortify.com')
-SHORTLINK_API = environ.get('SHORTLINK_API', 'c8c51280ae7b73b07b278a6ec131c3fb9d1598c5')
+SHORTLINK_API = environ.get('SHORTLINK_API', 'c1388492d1c69d38d15a0755f49592ff4b174ac4')
 TUTORIAL = environ.get('TUTORIAL', 'https://t.me/how_to_open_link_ak/18') # How Open Shortner Link Video Link , Channel Link Where You Upload Your Video.
 
 
