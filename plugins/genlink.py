1bbeeb4eac7e0086def11386b8df39bfbe171dd8
amit596 <kaliraman449@gmail.com>
2025-12-24 21:41:11 +0530

    Update genlink.py

--- plugins/genlink.py
+++ plugins/genlink.py
@@ -36,10 +36,10 @@ async def gen_link_s(bot, message):
 @Client.on_message(filters.command(['batch', 'pbatch']) & filters.create(allowed))
 async def gen_link_batch(bot, message):
     if " " not in message.text:
-        return await message.reply("Use correct format.\nExample <code>/batch https://t.me/cineoriginals/9 https://t.me/cineoriginals/164</code>.")
+        return await message.reply("Use correct format.\nExample <code>/batch https://t.me/akmovieshubx/9 https://t.me/akmovieshubx/164</code>.")
     links = message.text.strip().split(" ")
     if len(links) != 3:
-        return await message.reply("Use correct format.\nExample <code>/batch https://t.me/cineoriginals/9 https://t.me/cineoriginals/164</code>.")
+        return await message.reply("Use correct format.\nExample <code>/batch https://t.me/akmovieshubx/9 https://t.me/akmovieshubx/164</code>.")
     cmd, first, last = links
     regex = re.compile("(https://)?(t\.me/|telegram\.me/|telegram\.dog/)(c/)?(\d+|[a-zA-Z_0-9]+)/(\d+)$")
     match = regex.match(first)
@@ -114,3 +114,4 @@ async def gen_link_batch(bot, message):
     os.remove(f"batchmode_{message.from_user.id}.json")
     file_id, ref = unpack_new_file_id(post.document.file_id)
     await sts.edit(f"Here is your link\nContains `{og_msg}` files.\n https://t.me/{temp.U_NAME}?start=BATCH-{file_id}")
+
